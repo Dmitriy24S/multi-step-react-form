81b0a848d9cfc817eec1c2fd9a0a41aaa0d502a6
Dmitriy24S <devilins24@gmail.com>
2022-06-16 15:34:36 +0300

    style: :lipstick: make responsive, width, padding, spacing, custom tailwind xs breakpoint

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ function App() {
   return (
     <AppContext.Provider value={{ userData, setUserData, handleChange }}>
       <div className="App">
-        <section className="bg-slate-50 rounded-xl shadow-md w-[40rem] text-black p-12">
+        <section className="bg-slate-50 rounded-xl shadow-md w-full max-w-[36rem] md:w-1/2 md:min-w-[36rem] text-black px-4 py-12 xs:px-8 sm:px-12">
           {/* Progress bar */}
           <div className="progress-bar flex items-center mb-20">
             {/* Steps */}
@@ -99,7 +99,7 @@ function App() {
                   {/* progress line between checkpoints */}
                   {index > 0 && (
                     <div
-                      className={`progress-line w-full border-t-2 transition duration-300
+                      className={`progress-line  flex-auto border-t-2 transition duration-300
                   ${
                     index + 1 === step || index < step // old/current form step = green, future steps = gray
                       ? ` border-green-600`
@@ -110,9 +110,8 @@ function App() {
 
                   {/* Step number / checkmark done */}
                   <span
-                    className={`relative text-md rounded-full h-12 transition duration-300 ease-in-out ${
-                      index > 0 ? "w-16" : "w-12"
-                    } flex items-center justify-center border-2 border-gray-300 ${
+                    className={`relative text-md rounded-full h-12 transition duration-300 ease-in-out
+                    w-12 flex items-center justify-center border-2 border-gray-300 ${
                       index + 1 === step || index < step // old/current form step = green, future steps = gray
                         ? "border-green-600 text-white bg-green-600 font-bold "
                         : "text-slate-600"
@@ -120,9 +119,8 @@ function App() {
                   >
                     {index + 1 < step || step === 4 ? "✓" : index + 1}
                     {/* if passed step then checkmark, or if it is last step checkmark, otherwise uncompleted = show step number */}
-
                     {/* Title */}
-                    <div className="step-title absolute mt-24 t-0 w-32 text-xs font-medium uppercase text-gray-400 text-center">
+                    <div className="step-title absolute top-[115%] w-16 text-xs font-medium uppercase text-gray-400 text-center">
                       {item.title}
                     </div>
                   </span>
@@ -131,7 +129,10 @@ function App() {
             })}
           </div>
           {/* Form */}
-          <form className="flex flex-col text-lg gap-1" onSubmit={nextStep}>
+          <form
+            className="flex flex-col text-lg gap-1 mt-4"
+            onSubmit={nextStep}
+          >
             {displayStepForm(step)}
             <div className="button-container mt-12 flex justify-around text-lg">
               <button
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const { screens } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "420px",
+      ...screens,
+    },
     extend: {},
   },
   plugins: [],
